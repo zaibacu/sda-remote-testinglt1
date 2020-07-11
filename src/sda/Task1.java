4741001d1913af8b6b1b9d4a9ca71af65503bdb7
Šarūnas Navickas <zaibacu@gmail.com>
2020-07-11 09:24:39 +0300

    Task1 updated

--- src/sda/Task1.java
+++ src/sda/Task1.java
@@ -1,25 +1,42 @@
 package sda;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args){
-        int yearNow = 2020;
-        int birthYear = 2005;
+        Scanner scanner = new Scanner(System.in);
+        LocalDate now = LocalDate.now();
 
-        int monthNow = 6;
-        int monthBirth = 8;
+        int yearNow = now.getYear();
+        int birthYear;
+
+        int monthNow = now.getMonthValue();
+        int monthBirth;
+
+        System.out.println("What is your birth year?");
+        birthYear = scanner.nextInt();
 
         // > 18
-        if(yearNow - birthYear > 18){
+        int deltaYear = yearNow - birthYear;
+        if(deltaYear > 18){
             System.out.println("Here you go");
         }
         else{
             // < 18
-            if(yearNow - birthYear < 18){
+            if(deltaYear < 18){
                 System.out.println("Too young");
             }
             // = 18
             else {
-
+                System.out.println("What is your birth month?");
+                monthBirth = scanner.nextInt();
+                if(monthNow - monthBirth > 0){
+                    System.out.println("Here you go");
+                }
+                else {
+                    System.out.println("Too young");
+                }
             }
         }
     }
